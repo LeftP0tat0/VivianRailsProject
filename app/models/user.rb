e2888afd453916cb9a9df2cbc44daa180c41a167
Vivian stanley <vivianstanley@Vivians-MacBook-Air.local>
2021-07-25 22:21:47 -0700

    added some more things(sorry im tired and im not updating this correctly)`

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :Notes
     has_many :tags
     has_many :books
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root "sessions#home"
+
+  #signup
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  #login
+
+  #logout
+  delete '/logout' => 'sessions#destroy'
+
   resources :books
   resources :tags
   resources :users
